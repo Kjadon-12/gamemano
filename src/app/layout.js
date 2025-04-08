5dc24cc627a6af86b3471ec2cafcbbc844f0b379
Kanchan Jadon <kanchan.jadon@snva.com>
2025-04-08 19:43:42 +0530

    footer completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,14 @@
 
+import Footer from "@/components/Footer/Footer";
 import "./globals.css";
+import { Poppins } from 'next/font/google'
 
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '600', '700'], // choose weights you need
+  variable: '--font-poppins',   // optional for Tailwind custom font
+  display: 'swap',
+})
 
 
 export const metadata = {
@@ -10,11 +18,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={poppins.variable}>
       <body
         
       >
         {children}
+        <Footer/>
       </body>
     </html>
   );
